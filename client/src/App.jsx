5341631ab3b0e428101899a910cad26994d97f1e
Sameer <114623580+SamSwnshi@users.noreply.github.com>
2025-08-22 16:33:34 +0530

    resume page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,11 +13,10 @@ function App() {
       <main className="w-full  bg-[url('/images/bg-main.svg')] bg-cover">
         <Navbar />
         <section className="main-section">
-          <div className="page-heading">
+          <div className="page-heading py-16">
             <h1>Track Your Applications & Resume Ratings</h1>
             <h2>Review your submissions and check AI-powered feedback.</h2>
           </div>
-        </section>
 
         {resumes.length > 0 && (
           <div className="resumes-section">
@@ -26,6 +25,7 @@ function App() {
             ))}
           </div>
         )}
+        </section>
 
 
       </main>
--- client/src/components/ResumeCard.jsx
+++ client/src/components/ResumeCard.jsx
@@ -5,7 +5,7 @@ import ScoreCircle from './ScoreCircle'
 const ResumeCard = ({ resume }) => {
     console.log(resume)
     return (
-        <Link to={`/resume/${resume.id}`} className='resume-card animate-in fade-in-10 duration-1000'>
+        <Link to={`/resume/${resume.id}`} className='resume-card animate-in fade-in duration-1000'>
             <div className="resume-card-header">
                 <div className="flex flex-col gap-2">
                     <h2 className="!text-black font-bold break-words">{resume.companyName}</h2>
@@ -13,6 +13,11 @@ const ResumeCard = ({ resume }) => {
                 </div>
                 <div className="flex-shrink-0">
                     <ScoreCircle score={resume.feedback.overallScore} />
+                </div>
+            </div>
+            <div className="gradient-border animate-in fade-in duration-100">
+                <div className="w-full h-full">
+                    <img src={resume.imagePath} alt={resume.companyName} className='w-full h-[350px] max-sm:h-[200px] object-cover object-top'/>
                 </div>
             </div>
         </Link>
